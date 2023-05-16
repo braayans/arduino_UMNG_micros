a58897c21d05252ab763fc9b15451badf394e157
Brayan Romero <romero.ba.456@gmail.com>
2023-05-16 00:40:23 -0500

    Finalizacion de laboratorio 3

--- labCorte3/labCorte3.ino
+++ labCorte3/labCorte3.ino
@@ -11,10 +11,11 @@ volatile int decena = 0;
 
 //Variables de tiempo 
 volatile unsigned long int v_ini,v_ini2,v_ini3,v_ini4,v_act;
+unsigned long int flanco_subida;
 
 
 //Lista de recepcion de datos
-char dato_recibido = "a";
+int dato_recibido = 0;
 
 
 
@@ -27,6 +28,9 @@ const int pulsador1 = 18;
 const int pulsador2 = 19;
 const int pulsador3 = 2;
 const int pulsador4 = 3;
+const int pulsador5 = 10;
+int leer = 0;
+int lectura_anterior = 0;
 //--------------------------------------------------------------
 
 
@@ -37,43 +41,6 @@ volatile bool bandera_2 = false;
 volatile bool bandera_3 = false;
 volatile bool bandera_4 = false;
 //--------------------------------------------------------------
-
-
-
-
-//Variables para medir voltaje
-float voltaje_leido;
-int voltaje_real;
-short int contador_voltaje = 0;
-//--------------------------------------------------------------
-
-
-//Variables sensor luz
-const float Vin_luz = 4.99;
-const float Vmin_luz = 0.09;
-const float Vmax_luz = 4.89;
-const float R1_luz = 9700;
-float distancia_luz=(Vmax_luz-Vmin_luz)/100;
-float Vout_luz;
-int porcentaje_luz;
-//----------------------------------------------------------------
-
-
-//Variables de medidor resistencia
-short int contador_resistencia = 0;
-const float R1_resistencia = 9670;
-const float Vin_resistencia = 4.99;
-float Vout_resistencia = 0;
-int R2_resistencia;
-//----------------------------------------------------------------
-
-
-//Variables de temperatura----------------------------------------
-short int contador_temperatura = 0;
-float valor_temperatura;
-float Vout_temperatura;
-int temperatura_celsius;
-//-----------------------------------------------------------------
 
 
 
@@ -142,40 +109,9 @@ void pulsador_4_fun(){
   detachInterrupt(digitalPinToInterrupt(pulsador4));
 }
 
-
 //-------------------------------------------------------------------
 
 
-
-
-
-//Calculos de las variables en loop-------------------------------------
-void calculo_voltaje(){
-  voltaje_leido = (analogRead(A0)* 5)/1023.0;
-  voltaje_real = voltaje_leido * (50/5);
-}
-
-void calculo_resistencia(){
-  float valor_resistencia = analogRead(A1);
-  Vout_resistencia=valor_resistencia * 0.00488;
-  R2_resistencia = (Vout_resistencia * R1_resistencia) / (Vin_resistencia-Vout_resistencia);
-}
-
-void calculo_luz(){
-  float valor_luz = analogRead(A2);
-  Vout_luz = valor_luz*0.00488;
-  porcentaje_luz = 100 -( Vout_luz - Vmin_luz )/distancia_luz;
-  if(porcentaje_luz >= 100.0) porcentaje_luz = 100;
-  if(porcentaje_luz <= 0.0) porcentaje_luz = 0;
-}
-
-void calculo_temperatura(){
-  valor_temperatura = analogRead(A3);
-  Vout_temperatura = (valor_temperatura*5)/1023.0;
-  temperatura_celsius = Vout_temperatura*100;
-}
-
-//------------------------------------------------------------------------
 
 
 
@@ -188,14 +124,17 @@ void calculo_temperatura(){
 //--------------------------SETUP Y LOOP----------------------------------------------
 void setup() {
   Serial.begin(9600);
+  delay(1000);
   DDRA = 0XFF; //Pulsador de unidades
   DDRC = 0XFF; //Pulsador de decenas
+  DDRB = 0x00; //Para el ultimo pulsador
 
 
   pinMode(pulsador1,INPUT);
   pinMode(pulsador2,INPUT);
   pinMode(pulsador3,INPUT);
   pinMode(pulsador4,INPUT);
+  pinMode(pulsador5,INPUT);
 
   attachInterrupt(digitalPinToInterrupt(pulsador1), pulsador_1_fun, RISING);
   attachInterrupt(digitalPinToInterrupt(pulsador2), pulsador_2_fun, RISING);
@@ -218,23 +157,85 @@ void setup() {
 
 void loop() {
 
+  int voltaje_analogo = analogRead(A0);
+  voltaje_analogo = voltaje_analogo >> 2;
 
-  calculo_voltaje();
-  calculo_resistencia();
-  calculo_luz();
-  calculo_temperatura();
+  int temperatura_analogo = analogRead(A1);
+  temperatura_analogo = temperatura_analogo >> 2;
+
+  int resistencia_analogo = analogRead(A2);
+  resistencia_analogo = resistencia_analogo >> 2;
+
+  int luz_analogo = analogRead(A3);
+  luz_analogo = luz_analogo >> 2;
+
+
+
+
+
+
+  leer = digitalRead(pulsador5);
+  if (leer == HIGH && lectura_anterior == LOW && (millis() - flanco_subida) > 50 )
+  {  
+    Serial.write(5);
+    Serial.write(voltaje_analogo);
+    Serial.write(temperatura_analogo);
+    Serial.write(resistencia_analogo);
+    Serial.write(luz_analogo);   
+
+    flanco_subida = millis();
+
+  }  
+  lectura_anterior = leer;
+
 
 
 
   if (Serial.available()){
-    delay(5);
-    char  dato_recibido_pru = Serial.read();
-    Serial.print("Este es mi dato: ");
-    Serial.println(dato_recibido_pru);
 
-    if(dato_recibido_pru == 'm'){
-      PORTC = 0XFF;
+    delay(5);
+    dato_recibido = Serial.read();
+
+    if(dato_recibido == 1){
+      Serial.write(1);
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo);      
     }
+
+    if(dato_recibido == 2){
+      Serial.write(2);
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo);      
+    }
+
+    if(dato_recibido == 3){
+      Serial.write(3);
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo);      
+    }
+
+    if(dato_recibido == 4){
+      Serial.write(4);
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo);      
+    }
+
+    if(dato_recibido == 5){
+      Serial.write(5);
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo);      
+    }
+
 
   }
 
@@ -251,10 +252,10 @@ void loop() {
       PORTC = dato_SSD[decena];
 
       Serial.write(1);
-      Serial.write(voltaje_real);
-      Serial.write(temperatura_celsius);
-      Serial.write(R2_resistencia);
-      Serial.write(porcentaje_luz); 
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo); 
 
 
       v_ini = millis();
@@ -265,7 +266,7 @@ void loop() {
     }
   } 
 
-  //Segundo pulsador
+ //Segundo pulsador
  if(bandera_2 == true){
     v_act = millis();
     if(v_act - v_ini2 > 800){
@@ -273,10 +274,10 @@ void loop() {
       PORTC = dato_SSD[decena];
 
       Serial.write(2);
-      Serial.write(voltaje_real);
-      Serial.write(temperatura_celsius);
-      Serial.write(R2_resistencia);
-      Serial.write(porcentaje_luz); 
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo); 
 
       v_ini2 = millis();
       if(digitalRead(pulsador2) == 0){
@@ -286,17 +287,18 @@ void loop() {
     }
   } 
 
-  // Tercer pulsador
+ // Tercer pulsador 
  if(bandera_3 == true){
     v_act = millis();
     if(v_act - v_ini3 > 800){
       PORTA = dato_SSD[unidad];
       PORTC = dato_SSD[decena];
 
-      Serial.write(0x64);
-      Serial.print(4);
-      Serial.print(5);
-
+      Serial.write(3);
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo); 
 
 
       v_ini3 = millis();
@@ -307,8 +309,7 @@ void loop() {
     }
   } 
 
-
-  // Cuarto pulsador 
+ // Cuarto pulsador 
  if(bandera_4 == true){
     v_act = millis();
     if(v_act - v_ini4 > 800){
@@ -316,10 +317,10 @@ void loop() {
       PORTC = dato_SSD[decena];
 
       Serial.write(4);
-      Serial.write(voltaje_real);
-      Serial.write(temperatura_celsius);
-      Serial.write(R2_resistencia);
-      Serial.write(porcentaje_luz); 
+      Serial.write(voltaje_analogo);
+      Serial.write(temperatura_analogo);
+      Serial.write(resistencia_analogo);
+      Serial.write(luz_analogo); 
 
       v_ini4 = millis();
       if(digitalRead(pulsador4) == 0){
@@ -328,6 +329,8 @@ void loop() {
       }
     }
   } 
+
+
 
 
 
